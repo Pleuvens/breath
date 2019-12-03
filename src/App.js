f810615087a767947ac7dcf615890ba40d54b5b2
Pleuvens Fervil <pleuvens.fervil@gmail.com>
2019-12-03 10:35:22 +0100

    basic breather

--- src/App.js
+++ src/App.js
@@ -5,45 +5,46 @@ import BreathHelper from './breath-helper/BreathHelper';
 
 import './App.css';
 
+
+const steps = ['Inhale', 'Hold', 'Exhale', 'Hold']
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        toggle: true
+      step: 0
     };
     this.interval = null;
-}
-
-toggleMessage = () => {
-    this.setState({ toggle: !this.state.toggle });
-}
-
-componentDidMount() {
-    this.interval = setInterval(this.toggleMessage, 6000);
-}
-
-componentWillUnmount() {
-    clearInterval(this.interval);
-}
-  render() {
-    const toggle = this.state.toggle;        
-        return (
-            <div className="App">
-<Transition
-                items={toggle}
-                from={{ position: 'absolute', opacity: 0 }}
-                enter={{ opacity: 1 }}
-                leave={{ opacity: 0 }}>
-                {toggle =>
-                    toggle
-                    ? props =>  <BreathHelper animProps={props} msg="Inhale" />
-                    : props =>  <BreathHelper animProps={props} msg="Exhale" />
-                }
-            </Transition>
-            </div>
-            
-       )
   }
-}
+  
+  nextMessage = () => {
+    this.setState({ step: (this.state.step + 1) % steps.length });
+  }
+  
+  componentDidMount() {
+    this.interval = setInterval(this.nextMessage, 3700);
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  render() {
+    const step = this.state.step;        
+    return (
+      <div className="App">
+      <Transition
+      items={step}
+      from={{ position: 'absolute', opacity: 0 }}
+      enter={{ opacity: 1 }}
+      leave={{ opacity: 0 }}>
+      {step =>
+        props => <BreathHelper animProps={props} msg={steps[step]} />
+      }
+      </Transition>
+      </div>
+      
+      )
+    }
+  }
 
 export default App;
--- src/breath-helper/BreathHelper.js
+++ src/breath-helper/BreathHelper.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Spring } from 'react-spring/renderprops'
 
 import './BreathHelper.scss';
 
@@ -6,12 +7,28 @@ class BreathHelper extends React.Component {
 
     render() {
         return (
-            <div style={this.props.animProps} className="breath-helper">
+            <div style={this.props.animProps} className="breath-helper breath-helper-anim">
                 <div>{this.props.msg}</div>
-                <span class="top"></span>
-                <span class="right"></span>
-                <span class="bottom"></span>
-                <span class="left"></span>
+                <Spring
+                    from={{ transform: 'scaleX(0)' }}
+                    to={{ transform: 'scaleX(1)' }}>
+                    {props => <span style={props} className="top"></span>}
+                </Spring>
+                <Spring
+                    from={{ transform: 'scaleY(0)' }}
+                    to={{ transform: 'scaleY(1)' }}>
+                    {props => <span style={props} className="right"></span>}
+                </Spring>
+                <Spring
+                    from={{ transform: 'scaleX(0)' }}
+                    to={{ transform: 'scaleX(1)' }}>
+                    {props => <span style={props} className="bottom"></span>}
+                </Spring>
+                <Spring
+                    from={{ transform: 'scaleY(0)' }}
+                    to={{ transform: 'scaleY(1)' }}>
+                    {props => <span style={props} className="left"></span>}
+                </Spring>
             </div>
         )
     }
